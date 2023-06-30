fd2791ba48b2acd859cee7259acb71e5c90993a4
nicolas riera <nicolariera@gmail.com>
2023-06-30 10:19:57 +0200

    fix navigation with test

--- screens/PlayerDetailScreen.tsx
+++ screens/PlayerDetailScreen.tsx
@@ -1,46 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import DetailPlayerComponent from "../components/DetailPlayerComponent";
 
 import { PlayerDetail } from "../types";
 import { PlayerDetailScreenProps } from "../types";
 
-const PlayerDetailScreen: React.FC<PlayerDetailScreenProps> = ({ route }) => {
-  const [selectedPlayer, setSelectedPlayer] = useState<PlayerDetail | null>(
-    null
-  );
-  const [loading, setLoading] = useState(false);
+const PlayerDetailScreen = () => {
+  const navigation = useNavigation();
 
-  useEffect(() => {
-    const playerId = route.params?.playerId;
-    fetchPlayerDetail(playerId);
-  }, []);
-
-  const fetchPlayerDetail = async (playerId: string) => {
-    try {
-      const response = await fetch(
-        `https://api.mpg.football/api/data/championship-player-stats/${playerId}/2022`
-      );
-      const data = await response.json();
-      console.log("Player detail:", data);
-
-      setSelectedPlayer(data);
-    } catch (error) {
-      console.error("Failed to fetch player detail:", error);
-    }
+  const handleGoBack = () => {
+    navigation.goBack();
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Player Detail</Text>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : selectedPlayer ? (
-        <DetailPlayerComponent player={selectedPlayer} />
-      ) : (
-        <Text>No player selected</Text>
-      )}
-    </View>
+    <>
+      <View>
+        <TouchableOpacity onPress={handleGoBack}>
+          <Text style={styles.goback}>Retour</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Player Detail</Text>
+
+        <Text>Hola Test</Text>
+      </View>
+    </>
   );
 };
 
@@ -54,6 +39,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  goback: {
+    fontSize: 12,
+    color: "#7F7F7F",
+    fontWeight: "bold",
+    margin: 10,
   },
 });
 
---
+++ screens/SavePlayerDetailScreen.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import DetailPlayerComponent from "../components/DetailPlayerComponent";
+
+import { PlayerDetail } from "../types";
+import { PlayerDetailScreenProps } from "../types";
+
+const PlayerDetailScreen: React.FC<PlayerDetailScreenProps> = ({ route }) => {
+  const [selectedPlayer, setSelectedPlayer] = useState<PlayerDetail | null>(
+    null
+  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const playerId = route.params?.playerId;
+    fetchPlayerDetail(playerId);
+  }, []);
+
+  const fetchPlayerDetail = async (playerId: string) => {
+    try {
+      setLoading(true); // Set loading to true before making the API call
+
+      const response = await fetch(
+        `https://api.mpg.football/api/data/championship-player-stats/${playerId}/2022`
+      );
+      const data = await response.json();
+      console.log("Player detail:", data);
+
+      setSelectedPlayer(data);
+      setLoading(false); // Set loading to false after receiving the data
+    } catch (error) {
+      console.error("Failed to fetch player detail:", error);
+      setLoading(false); // Set loading to false in case of an error
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Player Detail</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : selectedPlayer ? (
+        <DetailPlayerComponent player={selectedPlayer} />
+      ) : (
+        <Text>No player selected</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
+
+export default PlayerDetailScreen;
